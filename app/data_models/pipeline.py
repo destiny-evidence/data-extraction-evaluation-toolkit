8d1897fc42b6292a049e6fc4a4e8e01e81bcc3fe
nikloynes <niklasloynes@gmail.com>
2025-10-23 13:49:40 +0100

    collating more pipeline ideas

--- app/data_models/pipeline.py
+++ app/data_models/pipeline.py
@@ -1,4 +1,4 @@
-"""Models to employ for implementing DEET tasks in sequential, harmonised fashion."""
+"""Models to employ for implementing DEET jobd in sequential, harmonised _pipelines_."""
 
 from enum import StrEnum, auto
 from pathlib import Path
@@ -12,6 +12,8 @@ class IngressMethod(StrEnum):
 
     FILE = auto()
     MEMORY = auto()
+    HTTP = auto()  # we may need to download data
+    RANDOM = auto()  # there might be jobs & pipeline stages where we start with a seed
 
 
 class EgressMethod(StrEnum):
@@ -21,9 +23,53 @@ class EgressMethod(StrEnum):
     MEMORY = auto()
 
 
-class InputFormat(BaseModel):
+class JobFormat(StrEnum):
     """
-    The format of incoming data.
+    An enum of job formats.
+
+    Jobs are the building blocks of pipeline stages.
+    The job format describes the 'medium' in which the job
+    is provided to the job object.
+    """
+
+    SCRIPT = auto()  # when job is supplied in a file.
+    CODE = auto()  # when job is supplied within the pipeline.
+
+
+class JobType(StrEnum):
+    """
+    An enum of job types.
+
+    This is a descriptive label of the broad category
+    of what the job is doing.
+    """
+
+    DATA_PROCESSING = auto()
+    DATA_COLLECTION = auto()
+    CLASSIFICATION = auto()
+    EXTRACTION = auto()
+
+
+class Language(StrEnum):
+    """An enum of permitted languages a job can be specified in."""
+
+    PYTHON = auto()
+    R = auto()
+    SHELL = auto()
+    SQL = auto()
+    LLM_PROMPT = auto()
+
+
+class JobExecutor:
+    """A wrapper around various tools for executing a job given job configuration."""
+
+    def __init__(self, language: Language, job_format: JobFormat):
+        pass
+
+
+class DataFormat(BaseModel):
+    """
+    The format data at a given stage of a job (ingress or egress).
 
     This is regardless of whether it is passed
     along from a previous stage in memory, or
@@ -38,6 +84,17 @@ class InputFormat(BaseModel):
     name: str | None
 
 
+class Job(BaseModel):
+    """The attributes describing a specific job."""
+
+    name: str
+    job_format: JobFormat
+    job_type: JobType | list[JobType]
+    language: Language
+    ingress_method: IngressMethod | None  # we may have a job that starts with no data
+    egress_method: EgressMethod
+
+
 class PipelineStage(BaseModel):
     """A stage in a DEET pipeline."""
 
@@ -47,6 +104,15 @@ class PipelineStage(BaseModel):
     permitted_egress_methods = list[EgressMethod]
     input_file: Path | None
     data: Any | None
+    jobs: Job | list[Job]
+
+    @classmethod
+    @field_validator("jobs", mode="before")
+    def convert_jobs_to_list(cls, v: Job | list[Job]) -> list[Job]:
+        """Convert jobs to list of jobs if just one job supplied."""
+        if isinstance(v, Job):
+            v = [v]
+        return v
 
     @classmethod
     @field_validator
