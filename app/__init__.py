657cc4dc8c23953350ad8c9be9413cfda3aba579
mcallaghan <max.w.callaghan@gmail.com>
2025-12-03 12:15:24 +0100

    fix linting error

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,3 @@
-from . import (
-    data_models,
-    extractors,
-    processors,
-    utils
-)
+from . import data_models, extractors, processors, utils
 
-__all__ = [
-    "data_models",
-    "extractors",
-    "processors",
-    "utils"
-]
+__all__ = ["data_models", "extractors", "processors", "utils"]
