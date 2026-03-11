49810d820347c8bae1bec759f93352a3b76c6a85
mcallaghan <max.w.callaghan@gmail.com>
2026-03-11 12:22:19 +0100

    check metrics display properly

--- tests/unit/test_gold_standard_llm_evaluator.py
+++ tests/unit/test_gold_standard_llm_evaluator.py
@@ -1,7 +1,9 @@
 import csv
+from unittest.mock import patch
 
 import pytest
 from loguru import logger
+from rich.table import Table
 
 from deet.evaluators.gold_standard_llm_evaluator import GoldStandardLLMEvaluator
 
@@ -99,3 +101,29 @@ def test_evaluator_writes_comparison(evaluator_evaluated, tmp_path):
     rows = list(reader)
     for r in rows:
         assert r["human_extraction"] == r["llm_extraction"]
+
+
+def test_evaluator_displays_metrics(evaluator_evaluated):
+    with patch(
+        "deet.evaluators.gold_standard_llm_evaluator.Console.print"
+    ) as mock_print:
+        evaluator_evaluated.display_metrics()
+
+    # ensure something was printed
+    assert mock_print.call_count == 1
+
+    table = mock_print.call_args[0][0]
+    assert isinstance(table, Table)
+
+    column_headers = [c.header for c in table.columns]
+    assert column_headers[0] == "Attribute"
+    for metric_name in evaluator_evaluated.metrics_config:
+        assert metric_name in column_headers
+
+    first_column_cells = table.columns[0]._cells
+    assert len(first_column_cells) == len(evaluator_evaluated.attributes)
+
+    metric_columns = table.columns[1:]
+    for col in metric_columns:
+        for cell in col._cells:
+            assert float(cell) == 1.0
