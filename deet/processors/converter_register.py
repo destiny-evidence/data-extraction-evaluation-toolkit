ad010ba1dec8a2343cc1c6eca4fe70b12476e6fb
Ishita Gopal <ishitagopal@gmail.com>
2026-03-23 14:25:26 +0100

    Add csv converter to register

--- deet/processors/converter_register.py
+++ deet/processors/converter_register.py
@@ -14,14 +14,23 @@ class SupportedImportFormat(StrEnum):
     """Supported formats to import gold standard annotation data from."""
 
     EPPI_JSON = auto()
+    GENERIC_CSV = auto()
 
     def get_annotation_converter(
         self,
     ) -> "AnnotationConverter":
         """Return an instance of the parser for the given data type."""
-        from deet.processors.eppi_annotation_converter import EppiAnnotationConverter
+        if self == SupportedImportFormat.EPPI_JSON:
+            from deet.processors.eppi_annotation_converter import (
+                EppiAnnotationConverter,
+            )
 
-        mapping = {
-            SupportedImportFormat.EPPI_JSON: EppiAnnotationConverter(),
-        }
-        return mapping[self]
+            return EppiAnnotationConverter()
+        if self == SupportedImportFormat.GENERIC_CSV:
+            from deet.processors.csv_annotation_converter import (
+                CSVAnnotationConverter,
+            )
+
+            return CSVAnnotationConverter()
+        msg = f"No annotation converter implemented for {self}"
+        raise ValueError(msg)
